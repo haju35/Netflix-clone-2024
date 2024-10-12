ff85ef22504128ec1f1151ff9065d309a6bbb6a8
haju35 <hajirasiraj217@gmail.com>
2024-10-12 00:32:37 -0700

    header and footer

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Haju Siraj</h1>
+      <Home/>
     </div>
   );
 }
